80895bc0f1e1b51063e6f462e1a6090b08c25a88
Filipp <filippstryukop@gmail.com>
2019-10-03 21:15:57 +0300

    gg

--- main/todoform.js
+++
@@ -1,8 +0,0 @@
-export default function TodoForm(props) {
-    return (
-        <form onSubmit={props.onSubmit}>
-            <input className="form-control" placeholder="Введите задачу" value={props.value} onChange={props.onChange}/>
-            <button type="submit" className="btn btn-primary my-2">Добавить задачу</button>
-        </form>
-    );
-}
